77e506c9343e960bad7ffdbe0e0a04b3bcd56318
SahilpuniaDits <sahilpuniawins@gmail.com>
2021-11-30 12:53:04 +0530

    uploadfile_add_crud

--- fileuplode/fileuplode/urls.py
+++ fileuplode/fileuplode/urls.py
@@ -21,5 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profile',views.uplodefile.as_view())
+    path('profile',views.uplodefile.as_view()),
+    path('profileCrud/<int:id>/',views.fileuploadcrud.as_view())
+
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- fileuplode/uplode_api/views.py
+++ fileuplode/uplode_api/views.py
@@ -6,6 +6,8 @@ from .models import profile
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import HttpResponse
+from rest_framework import status
 # Create your views here.
 
 
@@ -21,3 +23,30 @@ class uplodefile(APIView):
         data = profile.objects.all()
         serializer= profileSerializer(data,many=True)
         return Response(serializer.data)
+
+class fileuploadcrud(APIView):
+    
+    def get_object(self,id):
+        try:
+            return profile.objects.get(id=id)
+
+        except profile.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self,request,id):
+        students = self.get_object(id)
+        serializer = profileSerializer(students)
+        return Response(serializer.data)
+
+    def put(self,request,id):
+        students = self.get_object(id)
+        serializer = profileSerializer(students,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,id):
+        students = self.get_object(id)
+        students.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
